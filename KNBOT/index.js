f141ee65561f9ca89a3d6bcc73e97c1549c7db0d
Sharky20k <45876349+Sharky20k@users.noreply.github.com>
2018-12-14 14:34:35 +0100

    Add files via upload

---
+++ KNBOT/index.js
@@ -0,0 +1,30 @@
+const Commando = require('discord.js-commando')
+const client = new Commando.Client()
+
+client.registry.registerGroup('simple', 'Simple');
+client.registry.registerGroup('komendy', 'Komendy');
+client.registry.registerGroup('music', 'Music');
+client.registry.registerDefaults();
+client.registry.registerCommandsIn(__dirname + '/commands')
+
+global.servers = {};
+
+client.on('ready', () => {
+    console.log("jestem na serwerze jako " + client.user.tag)
+
+    client.user.setActivity("hentaje", {type: "WATCHING"})
+
+    client.guilds.forEach((guild) => {
+        console.log(guild.name)
+        guild.channels.forEach((channel) => {
+            console.log(` - ${channel.name} ${channel.type} ${channel.id}`)
+        })
+        // General channel id: 495679405985759240
+    })
+
+    let generalChannel = client.channels.get("495679405985759240")
+    generalChannel.send("Knutobot wbija na serwer. Listę komend znajdziesz pod !komendy")
+
+})
+
+client.login("NTIyNzgzNjI5MDk3NTY2MjEw.DvQFcw.iWf1wXTT36bIg9qDsXe-2-CUhk0")
